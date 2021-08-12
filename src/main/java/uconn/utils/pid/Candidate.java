d7d6108fce44f087b721378cfc7945a1b72e718c
Paul Simmerling <paul.simmerling@gmail.com>
2021-08-12 12:50:22 -0400

    Added Brandon's PID cuts

--- src/main/java/uconn/utils/pid/Candidate.java
+++ src/main/java/uconn/utils/pid/Candidate.java
@@ -41,6 +41,7 @@ public class Candidate {
     protected Integer dc_sector = null; ///< dc sector
     protected Short status = null; ///< particle status
 
+    protected Byte charge = null; ///< particle charge
 
     /**
      * set inbending magnetic field
@@ -111,6 +112,12 @@ public class Candidate {
         this.status = status==null ? null : status.shortValue();
     }
 
+    /**
+     * @param charge set particle charge
+     */
+    public void setCharge(Number charge) {
+        this.charge = charge==null ? null : charge.byteValue();
+    }
 
     /**
      * @param nphe set number of photoelectrons
