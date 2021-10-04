74fb4f0a2d460830a4d3e2e727999380f2fafec7
Andrey <kenjo@jlab.org>
2021-10-03 20:26:21 -0400

    update docs

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['field_0',['field',['../classuconn_1_1utils_1_1pid_1_1Candidate.html#aaf97a3841b5b5cf7fa9df6f1890b23a6',1,'uconn::utils::pid::Candidate']]],
-  ['forward_1',['FORWARD',['../classuconn_1_1utils_1_1pid_1_1stefan_1_1ProtonCandidate.html#a323f92a7201fca14f4d3f71296e01767',1,'uconn::utils::pid::stefan::ProtonCandidate::Cut']]]
+  ['forward_1',['FORWARD',['../classuconn_1_1utils_1_1pid_1_1stefan_1_1PhotonCandidate.html#a323f92a7201fca14f4d3f71296e01767',1,'uconn.utils.pid.stefan.PhotonCandidate.Cut.FORWARD()'],['../classuconn_1_1utils_1_1pid_1_1stefan_1_1ProtonCandidate.html#a323f92a7201fca14f4d3f71296e01767',1,'uconn.utils.pid.stefan.ProtonCandidate.Cut.FORWARD()']]]
 ];
