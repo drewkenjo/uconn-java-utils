3b2ed80cccb8883546bb5bf280b783258ce9db9a
Andrey <kenjo@jlab.org>
2021-10-20 11:34:41 -0400

    update docs

--- docs/search/all_9.js
+++ docs/search/all_9.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['level_0',['Level',['../classuconn_1_1utils_1_1pid_1_1Candidate.html#enumuconn_1_1utils_1_1pid_1_1Candidate_1_1Level',1,'uconn::utils::pid::Candidate']]],
-  ['loose_1',['LOOSE',['../classuconn_1_1utils_1_1pid_1_1Candidate.html#a230e91548381a7423a6bbeb6c4c7259c',1,'uconn::utils::pid::Candidate::Level']]]
+  ['loose_1',['LOOSE',['../classuconn_1_1utils_1_1pid_1_1Candidate.html#a230e91548381a7423a6bbeb6c4c7259c',1,'uconn::utils::pid::Candidate::Level']]],
+  ['loosest_2',['LOOSEST',['../classuconn_1_1utils_1_1pid_1_1Candidate.html#a4078a197f869050e0f17805a26897a53',1,'uconn::utils::pid::Candidate::Level']]]
 ];
